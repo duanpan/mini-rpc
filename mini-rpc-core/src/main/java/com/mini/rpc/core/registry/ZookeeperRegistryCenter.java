da5788abaf8b7045c8e18fecb3c236a21c51831c
pan.duan <pan.duan@tonoinfo.com>
2024-04-12 10:03:43 +0800

    优化代码

--- mini-rpc-core/src/main/java/com/mini/rpc/core/registry/ZookeeperRegistryCenter.java
+++ mini-rpc-core/src/main/java/com/mini/rpc/core/registry/ZookeeperRegistryCenter.java
@@ -1,19 +1,11 @@
 package com.mini.rpc.core.registry;
 
-import com.google.common.collect.Lists;
 import com.mini.rpc.core.constans.RpcConstans;
-import com.mini.rpc.core.consumer.ConsumerCache;
 import com.mini.rpc.core.entity.ProviderInstance;
-import com.mini.rpc.core.exception.RpcException;
-import com.mini.rpc.core.properties.RpcAppProperties;
-import com.mini.rpc.core.properties.RpcRegistryProperties;
-import com.mini.rpc.core.provider.ProviderCache;
-import com.mini.rpc.core.entity.ProviderMeta;
 import com.mini.rpc.core.entity.ServiceMeta;
-import com.mini.rpc.core.helper.MetaBuildHelper;
-import lombok.SneakyThrows;
+import com.mini.rpc.core.exception.RpcException;
+import com.mini.rpc.core.properties.RpcRegistryProperties;
 import lombok.extern.slf4j.Slf4j;
-import org.apache.commons.lang3.ObjectUtils;
 import org.apache.commons.lang3.StringUtils;
 import org.apache.curator.RetryPolicy;
 import org.apache.curator.framework.CuratorFramework;
@@ -23,10 +15,7 @@ import org.apache.curator.retry.ExponentialBackoffRetry;
 import org.apache.zookeeper.CreateMode;
 import org.springframework.beans.factory.annotation.Autowired;
 
-import java.util.ArrayList;
-import java.util.Iterator;
 import java.util.List;
-import java.util.Map;
 import java.util.stream.Collectors;
 
 /**
@@ -38,12 +27,9 @@ import java.util.stream.Collectors;
 @Slf4j
 public class ZookeeperRegistryCenter implements RegistryCenter {
 
-    private CuratorFramework curator;
-    @Autowired
-    private RpcAppProperties appProperties;
     @Autowired
     private RpcRegistryProperties registryProperties;
-
+    private CuratorFramework curator;
 
     @Override
     public void register(ServiceMeta serviceMeta) throws Exception {
@@ -83,11 +69,7 @@ public class ZookeeperRegistryCenter implements RegistryCenter {
 
     @Override
     public void nodeInit() throws Exception {
-        try {
-            doNodeInit(curator);
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+
     }
 
     @Override
@@ -95,7 +77,6 @@ public class ZookeeperRegistryCenter implements RegistryCenter {
         try {
             TreeCache cache = TreeCache.newBuilder(curator, RpcConstans.REGISTRY_NAMESPACE).setCacheData(true).setMaxDepth(2).build();
             cache.getListenable().addListener((client, event) -> {
-                doNodeInit(client);
             });
             cache.start();
         } catch (Exception e) {
@@ -106,44 +87,17 @@ public class ZookeeperRegistryCenter implements RegistryCenter {
     @Override
     public List<ProviderInstance> fetchServer(ServiceMeta serviceMeta) throws Exception {
         String serviceNodeName = serviceMeta.getNodeName();
-        List<String> nodes = null;
-        try {
-            nodes = curator.getChildren().forPath(serviceNodeName);
-        } catch (Exception e) {
-            throw new RpcException(e);
-        }
+        List<String> nodes = curator.getChildren().forPath(serviceNodeName);
 
         List<ProviderInstance> providerInstances = nodes.stream().map(node -> {
-            try {
-                ProviderInstance instance = new ProviderInstance();
-                instance.setIp(node.substring(0, node.indexOf(":")));
-                instance.setPort(node.substring(node.indexOf(":") + 1, node.length()));
-                instance.setProtocol(serviceMeta.getProtocol());
-                return instance;
-            } catch (Exception e) {
-                throw e;
-            }
+            ProviderInstance instance = new ProviderInstance();
+            instance.setIp(node.substring(0, node.indexOf(":")));
+            instance.setPort(node.substring(node.indexOf(":") + 1, node.length()));
+            instance.setProtocol(serviceMeta.getProtocol());
+            return instance;
         }).collect(Collectors.toList());
 
         return providerInstances;
     }
-
-    public void doNodeInit(CuratorFramework client) throws Exception {
-        String appPrefix = String.format("/%s/%s/%s", appProperties.getNamespace(), appProperties.getProtocol(),
-                appProperties.getEnv());
-        List<String> services = null;
-        services = client.getChildren().forPath(appPrefix);
-        for (String service : services) {
-            String servicePath = appPrefix.concat("/").concat(service);
-            List<String> nodes = client.getChildren().forPath(servicePath);
-            if (ConsumerCache.providesOnline.containsKey(service)) {
-                ConsumerCache.providesOnline.get(service).addAll(nodes);
-            } else {
-                ConsumerCache.providesOnline.put(service, Lists.newArrayList(nodes));
-            }
-        }
-
-    }
-
 
 }
